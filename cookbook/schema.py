8b6ab22d74f0741705cfe3e9e6a3704d1478e45f
Cristian Rojas <work@cristianrojas.com>
2017-08-22 14:54:42 -0500

    using relay

--- cookbook/schema.py
+++ cookbook/schema.py
@@ -1,9 +1,9 @@
 import graphene
 
-from ingredients import schema
+import ingredients.schema
 
-# You can think of this as being something like your top-level urls.py file
-class Query(schema.Query, graphene.ObjectType):
+
+class Query(ingredients.schema.Query, graphene.ObjectType):
     # This class will inherit from multiple Queries
     # as we begin to add more apps to our project
     pass
