099a277f63bfc418c933d444f3a4385d433062b6
Cristian Rojas <work@cristianrojas.com>
2017-08-22 15:40:01 -0500

    Login require example

--- ingredients/schema.py
+++ ingredients/schema.py
@@ -31,3 +31,12 @@ class Query(AbstractType):
 
     ingredient = relay.Node.Field(IngredientNode)
     all_ingredients = DjangoFilterConnectionField(IngredientNode)
+
+    def resolve_all_ingredients(self, args, context, info):
+        # If you are using GraphQLView you can access Django request with the context argument.
+        # context will reference to the Django request
+        # if not context.user.is_authenticated():
+        #     return Ingredient.objects.none()
+        # else:
+        #     return Ingredient.objects.filter(available=True)
+        return Ingredient.objects.filter(available=True)
