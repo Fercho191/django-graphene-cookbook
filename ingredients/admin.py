23e20e9bcb5a126264e097a9e462bccaad07d9e0
Cristian Rojas <work@cristianrojas.com>
2017-08-22 15:50:43 -0500

    Adding admin registers

--- ingredients/admin.py
+++ ingredients/admin.py
@@ -4,3 +4,18 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+from ingredients.models import Ingredient, Category
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    model = Ingredient
+    list_display = ('name', 'notes','category',)
+    search_fields = ('name', 'id',)
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    model = Category
+    list_display = ('name',)
+    search_fields = ('name', 'id',)
